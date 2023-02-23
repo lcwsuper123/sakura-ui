c154ff6940ef5cce49ac590229d6f5a7c6177b14
SakuraNoYume <2445263384@qq.com>
2023-02-23 22:15:36 +0800

    first commit

---
+++ packages/components/icon/src/icon.ts
@@ -0,0 +1,9 @@
+import {  ExtractPropTypes  } from 'vue'
+export const iconProps = {
+    size: {
+        type: Number
+    }, color: {
+        type: String
+    }
+} as const
+export type IconProps = ExtractPropTypes<typeof iconProps>
---
+++ packages/components/icon/src/icon.vue
@@ -0,0 +1,26 @@
+<template>
+    <i class="xbb-icon" :style="style">
+        <slot></slot>
+    </i>
+</template>
+<script lang="ts">
+export default {
+    name: 'sakuraIcon'
+}
+</script>
+<script setup lang="ts">
+import { computed, defineComponent } from 'vue'
+// import { IconProps } from './icon.ts'
+// const props = defineProps<IconProps>()
+// const style = computed(() => {
+//     if (!props.size && !props.color) {
+//         return {}
+//     }
+//     const style = {
+//         ...(props.size ? { 'font-size': props.size + 'px' } : {}),
+//         ...(props.color ? { 'color': props.color } : {}),
+//     }
+//     console.log('style', style)
+//     return style
+// })
+</script>
