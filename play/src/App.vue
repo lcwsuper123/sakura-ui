e3239fcf659d0dcfbe51407acde73e7be17a0351
SakuraNoYume <2445263384@qq.com>
2023-03-23 17:24:42 +0800

    theme

--- play/src/App.vue
+++ play/src/App.vue
@@ -1,5 +1,9 @@
 <template>
-    <s-text></s-text>
+    <!--<s-text></s-text>-->
+    <p class="s-title">
+        <span class="s-title__data">data1</span>
+        <span class="s-title__sex">sex</span>
+    </p>
 </template>
 
 <script setup lang="ts">
@@ -8,19 +12,38 @@ import SText from '@sakura-ui/components/text'
 </script>
 
 
-<style scoped>
-.logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+<style scoped lang="scss">
+$namespaces: 's';
+$common-separator: '-';
+$element-separator: '__';
+// s-text
+@mixin b($block) {
+    $B: $namespaces + $common-separator + $block !global;
+    .#{$B} {
+        @content;
+    }
 }
 
-.logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+// s-text__title
+@mixin e($element) {
+    $E: $element !global;
+    $selector: &;
+    $currentSelector: '';
+    @each $unit in $element {
+        $currentSelector: #{
+            $currentSelector + '.' + $B + $element-separator + $unit + ','
+        }
+    }
+    #{$currentSelector} {
+
+    }
+
 }
 
-.logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
+@include b(title) {
+    color: pink;
+    //@include e(data) {
+    //    color: pink;
+    //}
 }
 </style>
