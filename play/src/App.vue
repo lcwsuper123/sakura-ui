dc57cb0ad16f11dbf4da71d79e87e45ebe1e7372
SakuraNoYume <2445263384@qq.com>
2023-03-29 15:42:01 +0800

    funcation.scss完善与了解

--- play/src/App.vue
+++ play/src/App.vue
@@ -3,6 +3,11 @@
     <p class="s-title">
         <span class="s-title__data">data1</span>
         <span class="s-title__sex">sex</span>
+        <span class="s-title__sex--success">sex-success</span>
+        <span class="s-title__sex--error">sex-error</span>
+    </p>
+    <p class="s-title is-active">
+        <span class="s-title__data">when</span>
     </p>
 </template>
 
@@ -16,21 +21,30 @@ import SText from '@sakura-ui/components/text'
 //@import '@sakura-ui/theme-chalk/src/mixins/config.scss';
 //@import '@sakura-ui/theme-chalk/src/mixins/function.scss';
 @import '@sakura-ui/theme-chalk/src/mixins/mixins.scss';
+
 @include b(title) {
     color: pink;
+    display: flex;
+    flex-flow: column;
+    //#{getCssVarName('text', 'error', 'color')}: orangered;
     @include e(data) {
         color: red;
     }
+    @include m(success) {
+        color: yellowgreen;
+    }
+    @include e(sex) {
+        color: blue;
+        @include m(success) {
+            color: yellowgreen;
+        }
+    }
+    @include when(active){
+        background-color: pink;
+    }
 }
-@include b('title::before') {
-    content: "";
-    width: 100px;
-    height: 100px;
-    background-color: rebeccapurple;
-    display: block;
-}
-$demo: str-slice('.selector-data_title', 2, -2);
-#{$demo}{
-    width: 100px;
+.#{bem('title', 'sex', 'error')} {
+    //color: getCssVar('text', 'error', 'color');
+    color: getCssVarWithDefault(('text', 'error', 'color'), orange);
 }
 </style>
