ddc40c256f8758540808ee80c5b09e5f0d472bca
SakuraNoYume <2445263384@qq.com>
2023-05-23 10:53:46 +0800

    配置vitest环境

--- packages/components/button/__tests__/button.test.tsx
+++ packages/components/button/__tests__/button.test.tsx
@@ -2,12 +2,14 @@ import { markRaw, nextTick, ref } from 'vue'
 import { mount } from '@vue/test-utils'
 import { describe, expect, it, test } from 'vitest'
 import Button from '../src/button.vue'
-import type { ComponentSize } from '@sakura-ui/constants'
 
 describe('Button.vue', () => {
     it('create', () => {
-        const wrapper = mount(() => <Button type="primary"/>)
+        const wrapper = mount(() => <Button type="primary" />)
+
         expect(wrapper.classes()).toContain('s-button--primary')
-        // expect(true).toBe(true)
+    })
+    it('demo', () => {
+        expect(1).toBe(1)
     })
 })
